1ecb3ac74f46d65b105ee4f024c4ccc5b934757e
asejkinakseniya <asejkinak@gmail.com>
2025-10-25 23:03:41 +0300

    fix interface name in BannerCard

--- src/components/BannerCard.tsx
+++ src/components/BannerCard.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Carousel from "react-bootstrap/Carousel";
-import type { Banner } from "../types";
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import type { IBanner } from '../types';
 
-const BannerCard: React.FC<{ banner: Banner }> = ({ banner }) => {
-  return (
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={banner.image}
-        alt={`слайд ${banner.id}`}
-      />
-      <Carousel.Caption>
-        <h3>{banner.title}</h3>
-        <p>{banner.description}</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  );
+const BannerCard: React.FC<{ banner: IBanner }> = ({ banner }) => {
+	return (
+		<Carousel.Item>
+			<img
+				className="d-block w-100"
+				src={banner.image}
+				alt={`слайд ${banner.id}`}
+			/>
+			<Carousel.Caption>
+				<h3>{banner.title}</h3>
+				<p>{banner.description}</p>
+			</Carousel.Caption>
+		</Carousel.Item>
+	);
 };
 
 export default BannerCard;
